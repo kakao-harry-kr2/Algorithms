68b9e742b7be47bfa485b5a3887f4c7eeabc55b1
gksthdals <tiranosm@naver.com>
2022-01-23 23:59:14 +0900

    modify 20220123

--- 20220123/backjoon5373.py
+++ 20220123/backjoon5373.py
@@ -17,15 +17,6 @@ def rotate_counterclockwise(a):
             res[2-c][r] = a[r][c]
     return res
 
-def rotate(a, type):
-    if type == '+':
-        return rotate_clockwise(a)
-    else:
-        return rotate_counterclockwise(a)
-
-direction2int = {'U': 0, 'D': 1, 'F': 2, 'B': 3, 'L': 4, 'R': 5}
-directionChange = {'U': 'UFLBRD', 'D': 'DFRBLU', 'F': 'FURDLB', 'B': 'BULDRF', 'L': 'LUFDBR', 'R': 'RUBDFL'}
-
 for _ in range(T):
     # 큐브 초기화
     up =    [['w'] * 3 for _ in range(3)]
@@ -35,19 +26,52 @@ for _ in range(T):
     left =  [['g'] * 3 for _ in range(3)]
     right = [['b'] * 3 for _ in range(3)]
 
-    main_total = [up] + [down] + [front] + [back] + [left] + [right]
-
     n = int(input())
-    moveList = list(input().split())
+    moveList = list(input().rstrip().split())
 
     for move in moveList:
         space, type = move[0], move[1]
-        total = [main_total[direction2int[directionChange[space][i]]] for i in range(6)]
 
-        # 윗면 rotate
-        total[0] = rotate(total[0], type)
+        if space == 'U' and type == '+':
+            up = rotate_clockwise(up)
+            front[0], right[0], back[0], left[0] = right[0], back[0], left[0], front[0]
+        elif space == 'U' and type == '-':
+            up = rotate_counterclockwise(up)
+            front[0], right[0], back[0], left[0] = left[0], front[0], right[0], back[0]
+        elif space == 'D' and type == '+':
+            down = rotate_clockwise(down)
+            front[2], right[2], back[2], left[2] = left[2], front[2], right[2], back[2]
+        elif space == 'D' and type == '-':
+            down = rotate_counterclockwise(down)
+            front[2], right[2], back[2], left[2] = right[2], back[2], left[2], front[2]
+        elif space == 'F' and type == '+':
+            front = rotate_clockwise(front)
+            up[2][0], up[2][1], up[2][2], left[2][2], left[1][2], left[0][2], down[0][2], down[0][1], down[0][0], right[0][0], right[1][0], right[2][0] = left[2][2], left[1][2], left[0][2], down[0][2], down[0][1], down[0][0], right[0][0], right[1][0], right[2][0], up[2][0], up[2][1], up[2][2]
+        elif space == 'F' and type == '-':
+            front = rotate_counterclockwise(front)
+            up[2][0], up[2][1], up[2][2], left[2][2], left[1][2], left[0][2], down[0][2], down[0][1], down[0][0], right[0][0], right[1][0], right[2][0] = right[0][0], right[1][0], right[2][0], up[2][0], up[2][1], up[2][2], left[2][2], left[1][2], left[0][2], down[0][2], down[0][1], down[0][0]
+        elif space == 'B' and type == '+':
+            back = rotate_clockwise(back)
+            up[0][2], up[0][1], up[0][0], left[0][0], left[1][0], left[2][0], down[2][0], down[2][1], down[2][2], right[2][2], right[1][2], right[0][2] = right[2][2], right[1][2], right[0][2], up[0][2], up[0][1], up[0][0], left[0][0], left[1][0], left[2][0], down[2][0], down[2][1], down[2][2]
+        elif space == 'B' and type == '-':
+            back = rotate_counterclockwise(back)
+            up[0][2], up[0][1], up[0][0], left[0][0], left[1][0], left[2][0], down[2][0], down[2][1], down[2][2], right[2][2], right[1][2], right[0][2] = left[0][0], left[1][0], left[2][0], down[2][0], down[2][1], down[2][2], right[2][2], right[1][2], right[0][2], up[0][2], up[0][1], up[0][0]
+        elif space == 'L' and type == '+':
+            left = rotate_clockwise(left)
+            up[2][0], up[1][0], up[0][0], front[2][0], front[1][0], front[0][0], down[2][0], down[1][0], down[0][0], back[0][2], back[1][2], back[2][2] = back[0][2], back[1][2], back[2][2], up[2][0], up[1][0], up[0][0], front[2][0], front[1][0], front[0][0], down[2][0], down[1][0], down[0][0]
+        elif space == 'L' and type == '-':
+            left = rotate_counterclockwise(left)
+            up[2][0], up[1][0], up[0][0], front[2][0], front[1][0], front[0][0], down[2][0], down[1][0], down[0][0], back[0][2], back[1][2], back[2][2] = front[2][0], front[1][0], front[0][0], down[2][0], down[1][0], down[0][0], back[0][2], back[1][2], back[2][2], up[2][0], up[1][0], up[0][0]
+        elif space == 'R' and type == '+':
+            right = rotate_clockwise(right)
+            up[2][2], up[1][2], up[0][2], back[0][0], back[1][0], back[2][0], down[2][2], down[1][2], down[0][2], front[2][2], front[1][2], front[0][2] = front[2][2], front[1][2], front[0][2], up[2][2], up[1][2], up[0][2], back[0][0], back[1][0], back[2][0], down[2][2], down[1][2], down[0][2]
+        elif space == 'R' and type == '-':
+            right = rotate_counterclockwise(right)
+            up[2][2], up[1][2], up[0][2], back[0][0], back[1][0], back[2][0], down[2][2], down[1][2], down[0][2], front[2][2], front[1][2], front[0][2] = back[0][0], back[1][0], back[2][0], down[2][2], down[1][2], down[0][2], front[2][2], front[1][2], front[0][2], up[2][2], up[1][2], up[0][2]
+        else:
+            pass
 
-        # 옆면 rotate
-        rotate_type = 1 if type == '+' else -1
-
-        # Todo
+    for i in range(3):
+        for j in range(3):
+            print(up[i][j], end='')
+        print()

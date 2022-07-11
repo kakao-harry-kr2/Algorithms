4a01fafeea631900f31d009096752b208c6fa1b9
gksthdals <tiranosm@naver.com>
2022-07-11 22:18:22 +0900

    update 20220711

---
+++ 07-July/20220711/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+numList = list(map(int, input().split()))
+
+i, j = 0, K
+summation = answer = sum(numList[:K])
+
+while j < N:
+    summation += (numList[j] - numList[i])
+    answer = max(answer, summation)
+
+    i += 1
+    j += 1

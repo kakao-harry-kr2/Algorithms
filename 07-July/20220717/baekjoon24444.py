f3c42558ab9a354f192f0cf2849811016dd9c943
gksthdals <tiranosm@naver.com>
2022-07-17 19:53:25 +0900

    update 20220717

---
+++ 07-July/20220717/baekjoon24444.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for u in range(1, N+1):
+    graph[u] = sorted(graph[u])
+
+visited = [False] * (N+1)
+
+visited[R] = True
+q = deque([R])
+
+answer = [0] * (N+1)
+count = 0
+while q:
+    now = q.popleft()
+
+    count += 1
+    answer[now] = count
+
+    for next in graph[now]:
+        if not visited[next]:
+            visited[next] = True
+            q.append(next)
+
+for num in answer[1:]:
+    print(num)
